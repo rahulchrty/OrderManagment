d37f2a6239c082a25ae6a20581aabbe571f1eede
Rahul Chakrabarty <rahulchrty@gmail.com>
2020-07-03 02:34:54 +0530

    component BillingReyaltyDepartment

---
+++ OrderManagment.CLI/Implementations/BillingReyaltyDepartment.cs
@@ -0,0 +1,32 @@
+﻿using OrderManagment.CLI.Interfaces;
+using OrderManagment.CLI.Models;
+using System.Collections.Generic;
+
+namespace OrderManagment.CLI.Implementations
+{
+    public class BillingReyaltyDepartment : IBillable
+    {
+        private IBillable _bill;
+        public string BillingItemType { get; private set; } = "book";
+        public BillingReyaltyDepartment(IBillable bill)
+        {
+            _bill = bill;
+        }
+        public Billing GenerateBill(string itemName)
+        {
+            Billing billing = new Billing();
+            Billing physicalItem = _bill.GenerateBill(itemName);
+            Receipt royaltyDuplicate = new Receipt { 
+                TotalBillingAmout = physicalItem.Receipts[0].TotalBillingAmout,
+                BilligFor = physicalItem.Receipts[0].BilligFor,
+                BillTo = "Royalty Department",
+                Item = physicalItem.Receipts[0].Item
+            };
+            billing.Receipts = new List<Receipt>();
+            billing.Receipts.AddRange(physicalItem.Receipts);
+            billing.Receipts.Add(royaltyDuplicate);
+            billing.Commission = physicalItem.Commission;
+            return billing;
+        }
+    }
+}
---
+++ OrderManagment.Test/BillingReyaltyDepartmentTest.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using OrderManagment.CLI.Implementations;
+using OrderManagment.CLI.Interfaces;
+using OrderManagment.CLI.Models;
+using System.Collections.Generic;
+
+namespace OrderManagment.Test
+{
+    [TestClass]
+    public class BillingReyaltyDepartmentTest
+    {
+        private Mock<IBillable> _mockBill;
+        private BillingReyaltyDepartment _billingReyaltyDepartment;
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockBill = new Mock<IBillable>();
+            _billingReyaltyDepartment = new BillingReyaltyDepartment(_mockBill.Object);
+        }
+
+        //Given: An a book as a product
+        //When: I call GenerateBill
+        //Then: I get  a type of Billing
+        [TestMethod]
+        public void Given_Book_Itme()
+        {
+            string itemName = "some book";
+            Billing billing = new Billing
+            {
+                Receipts = new List<Receipt> { new Receipt {
+                TotalBillingAmout = 1f,
+                BilligFor = "Packing",
+                BillTo = "Customer",
+                Item = "book"} },
+                Commission = new Commission { CommissionAmmount = 1f, CommissionTo = "Agent" }
+            };
+            _mockBill.Setup(x => x.GenerateBill(It.IsAny<string>())).Returns(billing);
+            var result = _billingReyaltyDepartment.GenerateBill(itemName);
+            Assert.IsTrue(result is Billing);
+        }
+
+        //Given: An a book as a product
+        //When: I call GenerateBill
+        //Then: I get 2 receipts
+        [TestMethod]
+        public void Given_Book_Itme_Get_2_Receipts()
+        {
+            string itemName = "some book";
+            Billing billing = new Billing
+            {
+                Receipts = new List<Receipt> { new Receipt {
+                TotalBillingAmout = 1f,
+                BilligFor = "Packing",
+                BillTo = "Customer",
+                Item = "book"} },
+                Commission = new Commission { CommissionAmmount = 1f, CommissionTo = "Agent" }
+            };
+            _mockBill.Setup(x => x.GenerateBill(It.IsAny<string>())).Returns(billing);
+            var result = _billingReyaltyDepartment.GenerateBill(itemName);
+            Assert.AreEqual(result.Receipts.Count, 2);
+        }
+
+        //Given: An a book as a product
+        //When: I call GenerateBill
+        //Then: I get 2nd receipt BillTo as 'Royalty Department'
+        [TestMethod]
+        public void Given_Book_Itme_Get_2nd_Receipt_With_Royalty_Department()
+        {
+            string itemName = "some book";
+            Billing billing = new Billing
+            {
+                Receipts = new List<Receipt> { new Receipt {
+                TotalBillingAmout = 1f,
+                BilligFor = "Packing",
+                BillTo = "Customer",
+                Item = "book"} },
+                Commission = new Commission { CommissionAmmount = 1f, CommissionTo = "Agent" }
+            };
+            _mockBill.Setup(x => x.GenerateBill(It.IsAny<string>())).Returns(billing);
+            var result = _billingReyaltyDepartment.GenerateBill(itemName);
+            Assert.AreEqual(result.Receipts[1].BillTo, "Royalty Department");
+        }
+    }
+}
